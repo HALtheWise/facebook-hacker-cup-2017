a9209059184b2c8b545f70d85427200a2a18baa3
Eric Miller <eric@legoaces.org>
2017-01-21 23:28:42 -0500

    Old changes from pie_progress in round 2

--- Round-2/1-pie-progress/main.go
+++ Round-2/1-pie-progress/main.go
@@ -1,77 +1,129 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
 	"math"
 	"os"
 	"sort"
+	"strings"
 )
 
-const PROBLEM_NAME = "lazy_loading"
+const PROBLEM_NAME = "pie_progress"
 
 // Represents all of the information contained in the input file regarding a single
 // test case.
 type TestCase struct {
-	numBoxes int
-	weights  []int
+	//	testID              int
+	numDays, piesPerDay int
+	costPerPie          [][]int
+}
+
+type Circumstance struct {
+	t           *TestCase
+	piesOwned   int
+	daysElapsed int
+}
+
+var memoTable map[Circumstance]int
+
+// Note: we assume prices are sorted in ascending order
+func bestPossible(c Circumstance) (moneytospend int) {
+	moneytospend = math.MaxInt64
+
+	if memo, ok := memoTable[c]; ok {
+		return memo
+	}
+
+	if c.piesOwned >= c.t.numDays {
+		// Already bought enough pies
+		//		fmt.Printf("Win on %#v\n", c)
+		return 0
+	}
+	if c.daysElapsed > c.t.numDays {
+		//Time is out! You lose!
+		return math.MaxInt64
+	}
+	if c.daysElapsed > c.piesOwned {
+		// You die of starvation!
+		return math.MaxInt64
+	}
+
+	currentDay := c.t.costPerPie[c.daysElapsed]
+
+	boughtCost := 0
+	for buy := 0; buy <= c.t.piesPerDay; buy++ {
+		// Try buying this many pies
+		if buy > 0 {
+			boughtCost += currentDay[buy-1]
+		}
+		newCircumstance := c
+		newCircumstance.piesOwned += buy
+		newCircumstance.daysElapsed++
+		cost := boughtCost + bestPossible(newCircumstance) + buy*buy
+		if cost < moneytospend {
+			// We found a better solution!
+			moneytospend = cost
+		}
+	}
+
+	//	fmt.Println(c, moneytospend)
+
+	memoTable[c] = moneytospend
+	return moneytospend
+}
+
+// Solve a test case, returning a TestResult
+func (t TestCase) Solve() (cost TestResult) {
+	memoTable = make(map[Circumstance]int)
+	c := Circumstance{
+		t:           &t,
+		piesOwned:   0,
+		daysElapsed: 0,
+	}
+	return TestResult(bestPossible(c))
+}
+
+// Parse a single test case from an io.Reader, including consuming the newline
+// following the last line.
+func ParseCase(filein *bufio.Reader) (t TestCase, err error) {
+	//	t.testID = rand.Int()
+	_, err = fmt.Fscanf(filein, "%v %v\n", &t.numDays, &t.piesPerDay)
+	if err != nil {
+		return
+	}
+
+	t.costPerPie = make([][]int, t.numDays)
+
+	for i := range t.costPerPie {
+		t.costPerPie[i] = make([]int, t.piesPerDay)
+	}
+
+	for i := 0; i < t.numDays; i++ {
+		dayString, _ := filein.ReadString('\n')
+		costStrings := strings.Split(dayString, " ")
+		for j := 0; j < t.piesPerDay; j++ {
+			_, err = fmt.Sscanf(strings.TrimSpace(costStrings[j]), "%v", &t.costPerPie[i][j])
+			if err != nil {
+				return
+			}
+		}
+		sort.Ints(t.costPerPie[i])
+	}
+	return
 }
 
 // Represents all of the information regarding a single test case
 // that needs to be provided in the output file.
 type TestResult int
 
-// Parse a single test case from an io.Reader, including consuming the newline
-// following the last line.
-func ParseCase(filein io.Reader) (t TestCase, err error) {
-	_, err = fmt.Fscanf(filein, "%v\n", &t.numBoxes)
-	if err != nil {
-		return
-	}
-
-	t.weights = make([]int, t.numBoxes)
-
-	for i := 0; i < t.numBoxes; i++ {
-		_, err = fmt.Fscanf(filein, "%v\n", &t.weights[i])
-		if err != nil {
-			return
-		}
-	}
-	return
-}
-
-// Solve a test case, returning a TestResult
-func (t TestCase) Solve() (trips TestResult) {
-	boxStackHeights := make([]int, t.numBoxes)
-
-	for i := 0; i < t.numBoxes; i++ {
-		boxStackHeights[i] = int(math.Ceil(50.0 / float64(t.weights[i])))
-	}
-
-	sort.Ints(boxStackHeights)
-
-	fmt.Println(boxStackHeights)
-
-	usedBoxes := 0
-
-	for i := 0; i < t.numBoxes; i++ {
-		thisbox := boxStackHeights[i]
-		if usedBoxes+thisbox <= t.numBoxes {
-			trips++
-			usedBoxes += thisbox
-		} else {
-			return
-		}
-	}
-
-	return
-}
-
 func OutputResult(testID int, result TestResult) string {
 	return fmt.Sprintf("Case #%d: %v\n", testID+1, result)
 }
 
-func ParseCases(filein io.Reader) (ts []TestCase, err error) {
+func ParseCases(file io.Reader) (ts []TestCase, err error) {
+	filein := bufio.NewReader(file)
 	var numCases int
 	_, err = fmt.Fscanf(filein, "%v\n", &numCases)
 	if err != nil {
